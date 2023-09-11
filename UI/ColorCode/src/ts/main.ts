ea97e17effbe8f0c7e19afbf28f78a1d0c57e60a
Yehuda Yadid <yehudayadid@microsoft.com>
2023-09-11 23:00:48 +0300

    Improve

--- UI/ColorCode/src/ts/main.ts
+++ UI/ColorCode/src/ts/main.ts
@@ -5,52 +5,81 @@ import {drawSegmentation, clearSegmentationLayer} from "./layer-segmentation";
 import {createLabel, clearAllLabels} from "./layer-data";
 import {imageAnalyzeRequest, segmentationRequest} from "./api";
 
+const fullScreen = async () => {
+    const query = new URLSearchParams(location.search);
 
-onStartButtonClick(async () => {
-    await Promise.all([
-        startVideo(),
-        document.documentElement.requestFullscreen()
-    ]);
-
-    hideStartLayer();
-    showCaptureLayer();
-});
-
-onCaptureButtonClick(async () => {
-    pauseVideo();
-    toggleCaptureButton();
-
-    const blob = await captureVideoFrame();
-    if(!blob) {
-        alert("Failed to capture video frame");
-        return;
-    };
-
-    const [segmentation, analyze] = await Promise.all([
-        segmentationRequest(blob).then(res => res.json()),
-        imageAnalyzeRequest(blob).then(res => res.json()),
-    ]);
-
-    drawSegmentation(segmentation.imageSegmentationLabels);
-
-    const results = JSON.parse(analyze.result);
-    const positions = segmentation.boxes;
-
-    for(const pos of positions) {
-        const value = results[pos.label];
-
-        if(value && value !== "unknown") {
-            createLabel(pos.label, value, pos.x, pos.y);
-        }
+    if(query.get("fullScreen") !== "false") {
+        await document.documentElement.requestFullscreen();
     }
+}
 
-    toggleRestartButton();
-});
+const onCapture = async () => {
+    try {
+        pauseVideo();
+        toggleCaptureButton();
+    
+        const blob = await captureVideoFrame();
+        if(!blob) {
+            throw new Error("Failed to capture video frame");
+        }
+    
+        const [segmentation, analyze] = await Promise.all([
+            segmentationRequest(blob).then(res => res.json()),
+            imageAnalyzeRequest(blob).then(res => res.json()),
+        ]);
+    
+        drawSegmentation(segmentation.imageSegmentationLabels);
+    
+        const results = JSON.parse(analyze.result);
+        const positions = segmentation.boxes;
+    
+        for(const pos of positions) {
+            const value = results[pos.label];
+    
+            if(value && value !== "unknown") {
+                createLabel(pos.label, value, pos.x, pos.y);
+            }
+        }
+    
+        toggleRestartButton();
+    } catch(e) {
+        alert(e);
+    }
+}
 
-onRestartButtonClick(() => {
+const onRestart = () => {
     toggleRestartButton();
     clearAllLabels();
     clearSegmentationLayer();
     playVideo();
     toggleCaptureButton();
+}
+
+const onStart = async () => {
+    try {
+        await Promise.all([
+            startVideo(),
+            fullScreen(),
+        ]);
+    
+        hideStartLayer();
+        showCaptureLayer();
+    } catch(e) {
+        alert(e);
+    }
+}
+    
+
+
+window.addEventListener("keydown", (e) => {
+    if(e.key === "Enter" || e.key === "c") {
+        onCapture();
+    
+    } else if(e.key === "Escape" || e.key === "r") {
+        onRestart();
+    }
 });
+
+onCaptureButtonClick(onCapture);
+onRestartButtonClick(onRestart);
+onStartButtonClick(onStart);
