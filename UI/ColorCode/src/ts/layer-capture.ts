74b1f6453d87701193fab0040d5ff80094803ebf
Yehuda Yadid <yehudayadid@microsoft.com>
2023-09-13 13:38:02 +0300

    fix api and timer

--- UI/ColorCode/src/ts/layer-capture.ts
+++ UI/ColorCode/src/ts/layer-capture.ts
@@ -1,6 +1,34 @@
 const layer = document.getElementById('layer-capture');
 const captureBtn = document.getElementById('btn-capture');
 const restartBtn = document.getElementById('btn-restart');
+const timerElement = document.getElementById('timer');
+const errorMessage = document.getElementById('error-message');
+
+export const timer = (start = 3) => {
+    return new Promise(resolve => {
+        let count = start;
+
+        timerElement!.classList.remove("countdown-animation", "hide");
+        timerElement!.style.setProperty("--countdown-value", String(count));
+        timerElement!.classList.add("countdown-animation");
+        
+        const interval = setInterval(() => {
+            if(count === 0) {
+                clearInterval(interval);
+                timerElement!.classList.add("hide");
+                resolve(count);
+            }
+
+            timerElement!.classList.remove("countdown-animation");
+            timerElement!.style.setProperty("--countdown-value", String(--count));
+            timerElement!.classList.add("countdown-animation");
+        }, 1000);
+    });
+}
+
+export const toggleErrorMessage = () => {
+    errorMessage?.classList.toggle("hide");
+}
 
 export const showCaptureLayer = () => {
     layer?.classList.remove("hide");
